7b861e5307b395f6c4cf134a35cb4cf5a10145a8
hp <hp@example.com>
2023-05-14 02:24:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+document.querySelector('.banner button').addEventListener('click', () => {
+    document.querySelector('.banner').style.display = 'none';
+    document.querySelector('.slideshow-wrapper').style.cssText = 'opacity: 1; visibility: visible';
+    document.querySelector('.slideshow').style.animation = 'slideshow 24s infinite';
+});
+
+
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide(){
+	slides[currentSlide].className = 'slide';
+	currentSlide = (currentSlide+1)%slides.length;
+	slides[currentSlide].className = 'slide showing';
+}
+
+var playing = true;
+var pauseButton = document.getElementById('pause');
+
+function pauseSlideshow(){
+	pauseButton.innerHTML = 'Play';
+	playing = false;
+	clearInterval(slideInterval);
+}
+
+function playSlideshow(){
+	pauseButton.innerHTML = 'Pause';
+	playing = true;
+	slideInterval = setInterval(nextSlide,2000);
+}
+
+pauseButton.onclick = function(){
+	if(playing){ pauseSlideshow(); }
+	else{ playSlideshow(); }
+};
+
+
+
+
+
+
+
+
+
+
+
+
